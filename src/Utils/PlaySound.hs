9177dad8a3565c48b945f060373962291fa54ffc
Peter Trško <peter.trsko@gmail.com>
2018-01-07 17:52:22 +0000

    Updated `playSound` to work with `process` ≥ 1.2.0.0
    Data type `CreateProcess` changed a lot since `process` 1.2.0.0, some
    fields were uninitialised. Thankfully it also provides `proc` function,
    which is a smart constructor for `CreateProcess`, and only those fields
    that are different from defaults had to be manually changed.

--- src/Utils/PlaySound.hs
+++ src/Utils/PlaySound.hs
@@ -4,27 +4,20 @@ module Utils.PlaySound
     ) where
 
 import Control.Monad (void)
-import Data.Bool (Bool(False, True))
+import Data.Bool (Bool(True))
 import Data.Function (($), (.))
-import Data.Maybe (Maybe(Nothing))
 import Data.String (String)
 import System.IO (FilePath, IO, IOMode(ReadWriteMode), withFile)
 import System.Process
-    ( CmdSpec(RawCommand)
-    , CreateProcess
-        ( CreateProcess
-        , cmdspec
-        , std_in
+    ( CreateProcess
+        ( std_in
         , std_out
         , std_err
-        , cwd
-        , env
         , close_fds
-        , create_group
-        , delegate_ctlc
         )
     , StdStream(UseHandle)
     , createProcess
+    , proc
     )
 
 
@@ -34,16 +27,11 @@ player = "mplayer"
 playerOpts :: FilePath -> [String]
 playerOpts x = ["-really-quiet", x]
 
-playSound :: String -> IO ()
+playSound :: FilePath -> IO ()
 playSound x = void . withFile "/dev/null" ReadWriteMode $ \ h ->
-    createProcess CreateProcess
-        { cmdspec = RawCommand player $ playerOpts x
-        , std_in = UseHandle h
+    createProcess $ (proc player (playerOpts x))
+        { std_in = UseHandle h
         , std_out = UseHandle h
         , std_err = UseHandle h
-        , cwd = Nothing
-        , env = Nothing
         , close_fds = True
-        , create_group = False
-        , delegate_ctlc = False
         }
